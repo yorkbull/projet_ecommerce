be65cdf164857910292436f8117f40cd5ba599c9
joubert charles <charles.joubert@hetic.net>
2021-05-28 21:58:09 +0200

    bouton avis client

--- js/animation.js
+++ js/animation.js
@@ -1,12 +1,30 @@
 
 let togg1 = document.getElementById("togg1");
+let togg2 = document.getElementById("togg2");
+let togg3 = document.getElementById("togg3");
 
 let d1 = document.getElementById("avis_client_livraison");
+let d2 = document.getElementById("avis_client_produit");
+let d3 = document.getElementById("avis_client_sav");
 
 togg1.addEventListener("click", () => {
   if(getComputedStyle(d1).display != "none"){
     d1.style.display = "none";
   } else {
     d1.style.display = "block";
+  }
+})
+togg2.addEventListener("click", () => {
+  if(getComputedStyle(d2).display != "none"){
+    d2.style.display = "none";
+  } else {
+    d2.style.display = "block";
+  }
+})
+togg3.addEventListener("click", () => {
+  if(getComputedStyle(d3).display != "none"){
+    d3.style.display = "none";
+  } else {
+    d3.style.display = "block";
   }
 })
