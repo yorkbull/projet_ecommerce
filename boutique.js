dfe91d456bafae26398fdd4e930ef062046ab848
joubert charles <charles.joubert@hetic.net>
2021-05-30 22:27:30 +0200

    json

--- boutique.js
+++ boutique.js
@@ -15,9 +15,15 @@ function showArticle(jsonObj) {
   var article = jsonObj["article"];
   console.log(article)
   article.map((element)=>{
+    let image = document.createElement('img')
     let text = document.createElement('p')
+    let name = document.createElement('p')
+    image.innerHTML =+ "src="+ element.img_src
     text.innerHTML = element.unit_price
+    name.innerHTML = element.name
+    document.querySelector('article').appendChild(image)
     document.querySelector('article').appendChild(text)
+    document.querySelector('article').appendChild(name)
  })
 }
 
